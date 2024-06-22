decded9726373f38b0669b7b24ff0765a3952dd4
manojkumar <manoj.ravi94@gmail.com>
2024-06-22 17:02:29 +0530

    login reg

---
+++ lib/RegistionScreeen.dart
@@ -0,0 +1,186 @@
+import 'package:e_gold/LoginScreen.dart';
+import 'package:flutter/material.dart';
+
+// Import your home screen
+
+class RegistrationScreen extends StatefulWidget {
+  @override
+  _RegistrationScreenState createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+  final _formKey = GlobalKey<FormState>();
+  String _username = '';
+  String _email = '';
+  String _password = '';
+  bool _termsAccepted = false;
+  String _gender = '';
+  String _country = 'USA'; // Default selected country
+  List<String> _countries = ['USA', 'Canada', 'Mexico']; // Example countries
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Register'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: ListView(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Username',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter your username';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) => _username = value!,
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    border: OutlineInputBorder(),
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter your email';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) => _email = value!,
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    border: OutlineInputBorder(),
+                  ),
+                  obscureText: true,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter your password';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) => _password = value!,
+                ),
+                SizedBox(height: 20),
+                DropdownButtonFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Country',
+                    border: OutlineInputBorder(),
+                  ),
+                  value: _country,
+                  onChanged: (newValue) {
+                    setState(() {
+                      _country = newValue!;
+                    });
+                  },
+                  items: _countries.map((country) {
+                    return DropdownMenuItem(
+                      child: Text(
+                        country,
+                        style: TextStyle(color: Colors.red),
+                      ),
+                      value: country,
+                    );
+                  }).toList(),
+                ),
+                SizedBox(height: 20),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Gender',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    ListTile(
+                      title: const Text('Male'),
+                      leading: Radio<String>(
+                        value: 'Male',
+                        groupValue: _gender,
+                        onChanged: (value) {
+                          setState(() {
+                            print(value);
+                            _gender = value!;
+                          });
+                        },
+                      ),
+                    ),
+                    ListTile(
+                      title: const Text('Female'),
+                      leading: Radio<String>(
+                        value: 'Female',
+                        groupValue: _gender,
+                        onChanged: (value) {
+                          setState(() {
+                            print(value);
+                            _gender = value!;
+                          });
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+                CheckboxListTile(
+                  title: Text('I accept the terms and conditions'),
+                  value: _termsAccepted,
+                  onChanged: (value) {
+                    setState(() {
+                      _termsAccepted = value!;
+                    });
+                  },
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: _register,
+                  child: Text('Register'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _register() {
+    if (_formKey.currentState!.validate() &&
+        _termsAccepted &&
+        _gender.isNotEmpty) {
+      _formKey.currentState!.save();
+      // Perform registration logic with _username, _email, _password, _country, and _gender
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => LoginScreen()),
+      );
+    } else if (!_termsAccepted) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text('You must accept the terms and conditions'),
+        backgroundColor: Colors.red,
+        showCloseIcon: true,
+        action: SnackBarAction(
+          label: 'Ok',
+          onPressed: () {},
+        ),
+        duration: Duration(seconds: 30),
+      ));
+    } else if (_gender.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Please select your gender')),
+      );
+    }
+  }
+}
--- lib/SplashScreen.dart
+++ lib/SplashScreen.dart
@@ -1,47 +1,81 @@
+import 'package:e_gold/Glassmarphishm.dart';
 import 'package:e_gold/LoginScreen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
+import 'dart:async';
+// Import your home screen
 
+class SplashScreen extends StatefulWidget {
   @override
-  State<SplashScreen> createState() => _SplashScreenState();
+  _SplashScreenState createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
-  var fromLogin;
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController? _controller;
+  Animation<double>? _animation;
+
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    startCount().then((value) => print("value from then $value"));
+
+    _controller = AnimationController(
+      duration: const Duration(seconds: 5),
+      vsync: this,
+    );
+
+    _animation = Tween<double>(begin: 0.0, end: 0.9).animate(_controller!)
+      ..addListener(() {
+        setState(() {});
+      });
+
+    _controller!.forward();
+
+    Timer(Duration(seconds: 5), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => LoginScreen()),
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller!.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset('Assets/logo.jpg'),
-              Text("$fromLogin"),
-            ],
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                    'Assets/background.png'), // Add your background image here
+                fit: BoxFit.cover,
+              ),
+            ),
           ),
-        ),
+          GlassmorphicContainer(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ScaleTransition(
+                    scale: _animation!,
+                    child: Image.asset('Assets/logo.png'), // Your logo here
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
-  }
-
-  Future<bool> startCount() async {
-    bool isTrue = false;
-    await Future.delayed(Duration(seconds: 5)).then((value) {
-      print("value $value");
-      isTrue = true;
-      Navigator.pushNamed(context, '/login');
-    });
-    return isTrue;
   }
 }
